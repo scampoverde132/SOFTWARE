81bb5f3c66bd5936b71fd2c6c7647a2fc2d5a058
scampoverde132 <samucj1991@gmail.com>
2026-07-27 00:52:26 -0400

    Load bid finalization before app startup

--- js/pdf-loader.js
+++ js/pdf-loader.js
@@ -1,10 +1,11 @@
 /**
  * PlanTakeoff pre-app bootstrap.
- * Load disk-backed Jobs, Command Center, painting productivity, then PDF support.
+ * Load disk-backed Jobs, Command Center, productivity/finalization, then PDF support.
  */
 (function () {
   document.write('<script src="js/job-model.js"><\/script>');
   document.write('<script src="js/command-center.js"><\/script>');
   document.write('<script src="js/productivity.js"><\/script>');
+  document.write('<script src="js/finalization.js"><\/script>');
   document.write('<script src="js/pdf-loader-core.js"><\/script>');
 })();
