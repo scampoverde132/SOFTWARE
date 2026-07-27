5b2018d0d1e6585a8f27bc183dd81ceb22148097
scampoverde132 <samucj1991@gmail.com>
2026-07-27 00:29:46 -0400

    feat: load command center before app startup

--- js/pdf-loader.js
+++ js/pdf-loader.js
@@ -1,9 +1,10 @@
 /**
  * PlanTakeoff pre-app bootstrap.
- * Load disk-backed Jobs and painting productivity, then the unchanged PDF implementation.
+ * Load disk-backed Jobs, Command Center, painting productivity, then PDF support.
  */
 (function () {
   document.write('<script src="js/job-model.js"><\/script>');
+  document.write('<script src="js/command-center.js"><\/script>');
   document.write('<script src="js/productivity.js"><\/script>');
   document.write('<script src="js/pdf-loader-core.js"><\/script>');
 })();
