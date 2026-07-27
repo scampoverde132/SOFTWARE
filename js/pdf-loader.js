814d92898492eab90ac0dff0a8869cb1bebc57fb
scampoverde132 <samucj1991@gmail.com>
2026-07-27 01:29:39 -0400

    Load change order workflow before app startup

--- js/pdf-loader.js
+++ js/pdf-loader.js
@@ -1,11 +1,13 @@
 /**
  * PlanTakeoff pre-app bootstrap.
- * Load disk-backed Jobs, Command Center, productivity/finalization, then PDF support.
+ * Load disk-backed Jobs, Command Center, productivity/finalization/change orders,
+ * then the unchanged PDF implementation.
  */
 (function () {
   document.write('<script src="js/job-model.js"><\/script>');
   document.write('<script src="js/command-center.js"><\/script>');
   document.write('<script src="js/productivity.js"><\/script>');
   document.write('<script src="js/finalization.js"><\/script>');
+  document.write('<script src="js/change-order.js"><\/script>');
   document.write('<script src="js/pdf-loader-core.js"><\/script>');
 })();
